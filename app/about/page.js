528c6698dd895e7c3181c97506f2f42588c04387
ishoef <ismailhossennayeb@gmail.com>
2025-10-02 16:50:49 +0600

    added two pages

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            this is the about page of an-nahdah online isntitute
+        </div>
+    );
+}
+
+export default Page;
---
+++ app/contact/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>this is the contact page of an-nahdah online institute</div>;
+};
+
+export default Page;
