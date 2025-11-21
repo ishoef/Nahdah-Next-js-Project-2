4b4586db16588d77f80f0d38dab213a0c76293c9
ishoef <ismailhossennayeb@gmail.com>
2025-11-22 00:26:50 +0200

    some course added

--- components/home-components/featured-courses.jsx
+++ components/home-components/featured-courses.jsx
@@ -6,7 +6,10 @@ import CourseCard from "../ui/CourseCard";
 import { Filter } from "lucide-react";
 
 const FeaturedCourses = () => {
-  const allCourses = courseData.allCourses;
+  const sixTopCourse = courseData.allCourses
+    .filter((course) => course.rating >= 4.5)
+    .sort((a, b) => b.rating - a.rating)
+    .slice(0, 6);
 
   return (
     <section className="py-16 lg:py-18 bg-gradient-to-br from-blue-50 via-blue-100 to-blue-50 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 transition-colors duration-500">
@@ -23,7 +26,7 @@ const FeaturedCourses = () => {
       />
       <section className="py-10 lg:py-12 ">
         <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          {allCourses.length === 0 ? (
+          {sixTopCourse.length === 0 ? (
             <div className="text-center py-12">
               <Filter className="h-16 w-16 text-muted-foreground mx-auto mb-4" />
               <h3 className="text-xl font-semibold mb-2">No courses found</h3>
@@ -33,7 +36,7 @@ const FeaturedCourses = () => {
             </div>
           ) : (
             <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-              {allCourses.map((course) => (
+              {sixTopCourse.map((course) => (
                 <CourseCard key={course._id} course={course} />
               ))}
             </div>
